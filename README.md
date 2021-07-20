6bc356424e0e98d99ab2c31ccc8dd92733f972d4
Michel Maas <m.maas@btcdirect.eu>
2021-07-20 15:53:55 +0200

    Adds some explanation and comments, and the executable

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # scream
 Small Go project for fun times.
+
+## Building
+
+`go build -o scream main.go`
+
+## Usage
+
+- `scream text you want to scream`, e.g.: `scream help` outputs `:alphabet-white-h::alphabet-white-e::alphabet-white-l::alphabet-white-p:  :alphabet-white-m::alphabet-white-e:`
+- You could be like me and make a symlink to `/usr/bin/scream` by doing `ln -s ./scream /usr/bin/scream`
--- main.go
+++ main.go
@@ -12,14 +12,17 @@ func main() {
     for i := 0; i < len(args); i++ {
         letters := strings.Split(args[i], "")
 
+        // Loop over the letters in the word
         for _, letter := range letters {
             fmt.Print(":alphabet-white-" + letter + ":")
         }
 
+        // If we are not at the final word, add a couple of spaces
         if i != (len(args) - 1) {
             fmt.Print("  ")
         }
     }
 
+    // Print an empty line for easy copying in the terminal
     fmt.Println()
 }
